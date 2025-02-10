126f8a0b3271f31851a328df2a2b7431174f3f79
Aadhiraiyan <aadhiraiyan.tamilselvan@gmail.com>
2025-02-09 18:46:49 -0800

    aadhirs changes

---
+++ aadhir/coral_reef.py
@@ -0,0 +1,110 @@
+from hub import light_matrix, port, motion_sensor
+import motor
+import runloop
+import math
+import motor_pair
+from math import *
+
+WHEEL_CIRCUMFERENCE = 17.5
+DISTANCE_BETWEEN_WHEELS = 14.2 #cm - please measure your own robot.
+
+motor_pair.pair(motor_pair.PAIR_1, port.A, port.E)
+drive_motor_pair = motor_pair.PAIR_1
+default_velocity = 30
+turn_velocity= 100
+
+# input must be in the same unit as WHEEL_CIRCUMFERENCE
+def convert_distance_to_degree(distance_cm):
+    # Add multiplier for gear ratio if needed
+    return int((distance_cm/WHEEL_CIRCUMFERENCE) * 360)
+
+# input must be in the same unit as WHEEL_CIRCUMFERENCE
+def move_for_distance(distance_cm):
+        degrees = convert_distance_to_degree(distance_cm)
+        return motor_pair.move_for_degrees(drive_motor_pair,degrees,0)
+
+
+PIVOT_CIRCUMFERENCE = 2 * DISTANCE_BETWEEN_WHEELS * math.pi
+async def pivot_turn(robot_degrees, motor_speed=turn_velocity):
+    # Add a multiplier for gear ratios if you’re using gears
+    motor_degrees = int((PIVOT_CIRCUMFERENCE/WHEEL_CIRCUMFERENCE) * abs(robot_degrees))
+    if robot_degrees > 0:
+        # pivot clockwise
+        await motor_pair.move_for_degrees(motor_pair.PAIR_1, motor_degrees, 50, velocity=motor_speed)
+    else:
+        #pivot counter clockwise
+        await motor_pair.move_for_degrees(motor_pair.PAIR_1, motor_degrees, -50, velocity=motor_speed)
+
+# Function that returns true when the absolute yaw angle is 90 degrees
+def turn_done():
+    # convert tuple decidegree into same format as in app and blocks
+    return abs(motion_sensor.tilt_angles()[0] * -0.1) > 90
+
+async def spin_turn(degrees):
+    if abs(degrees) > 355:
+        print("Out of range")
+        return
+    motion_sensor.reset_yaw(0)
+    await runloop.until(motion_sensor.stable)
+    global degrees_to_turn, stop_angle
+    degrees_to_turn = degrees # set the global to use in the turn_done function
+    # set the stop_angle global to use in the turn_done function
+    if (abs(degrees) < 180):
+        stop_angle = abs(degrees_to_turn)
+    else:
+        stop_angle = (360 - abs(degrees)) if degrees < 0 else (abs(degrees) - 360)
+    # set the steering laue based on turn direction
+    steering_val = 100 if degrees >= 0 else -100
+    motor_pair.move(motor_pair.PAIR_1, steering_val, velocity=200)
+    await runloop.until(turn_done)
+    motor_pair.stop(motor_pair.PAIR_1)
+
+async def release_shark():
+    #reset arm
+    await motor.run_for_degrees(port.D, 150, 720)
+
+    #move
+    await pivot_turn(18)
+    await move_for_distance(42)
+    await pivot_turn(-55)
+    #await spin_turn(-47)
+    await move_for_distance(1)
+    await runloop.sleep_ms(1000)
+
+    #release shark
+    await motor.run_for_degrees(port.D, -150, 1000)
+    await runloop.sleep_ms(1000)
+    await motor.run_for_degrees(port.D, 150, 720)
+    #come back to base
+    await runloop.sleep_ms(1000)
+    await move_for_distance(-5)
+    await pivot_turn(55)
+    await move_for_distance(-52)
+    await pivot_turn(-18) 
+async def move_to_shark_habitat():
+    #hold the shark
+    await motor.run_for_degrees(port.D,-90, 720)
+    #move the shark to habitat
+    await move_for_distance(45)
+    #release shark
+    await motor.run_for_degrees(port.D,90, 720)
+async def collect_coral_and_return_to_base():
+    #collect first coral
+    await move_for_distance(13)
+    await pivot_turn(-19)
+    await move_for_distance(30)
+    #collect second coral and third coral
+    await pivot_turn(-30)
+    await move_for_distance(10)
+    await pivot_turn(-10)
+    await move_for_distance(40)
+    await pivot_turn(25)
+    await motor.run_for_degrees(port.D,-90,720)
+    await motor.run_for_degrees(port.D, 90,720)
+    await pivot_turn(-40)
+    await move_for_distance(25)
+    await spin_turn(-70)
+    await move_for_distance(20)
+    await spin_turn(90)
+    await move_for_distance(20)
+runloop.run(collect_coral_and_return_to_base())
