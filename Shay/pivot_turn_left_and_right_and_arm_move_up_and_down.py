078a2a497934bf76f99429a80130d50a5e6ac1bb
Shay-Mittal <shaykmittal@gmail.com>
2025-09-13 15:16:42 -0700

    pivot_turn_left_and_right_and_arm_move_up_and_down.py
    We might not need this code anymore.

--- Shay/pivot_turn_left_and_right_and_arm_move_up_and_down.py
+++ Shay/pivot_turn_left_and_right_and_arm_move_up_and_down.py
@@ -8,8 +8,8 @@ async def move_arm_up(degrees):
 async def move_arm_down(degrees):
     await motor.run_for_degrees(port.D, degrees, -360)
 motor_pair.pair(motor_pair.PAIR_1, port.A, port.E)
+#convert distance to degree is not part of the project, I just added it just in case
 def convert_distance_to_degree(distance_cm):
-    # Add multiplier for gear ratio if needed
     return int((distance_cm/17.5) * 360)
 async def pivot_turn_right(degrees, motor_speed):
     motor_degrees = int((PIVOT_CIRCUMFERENCE/WHEEL_CIRCUMFERENCE) * abs(degrees))
