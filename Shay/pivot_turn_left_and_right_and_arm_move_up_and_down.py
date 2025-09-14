8b50cdfab75997c242a5b367544729f102f1fd52
Shay-Mittal <shaykmittal@gmail.com>
2025-09-13 17:09:42 -0700

    Update pivot_turn_left_and_right_and_arm_move_up_and_down.py

--- Shay/pivot_turn_left_and_right_and_arm_move_up_and_down.py
+++ Shay/pivot_turn_left_and_right_and_arm_move_up_and_down.py
@@ -11,6 +11,9 @@ motor_pair.pair(motor_pair.PAIR_1, port.A, port.E)
 #convert distance to degree is not part of the project, I just added it just in case
 def convert_distance_to_degree(distance_cm):
     return int((distance_cm/17.5) * 360)
+async def move_for_distance(distance):
+    degrees = convert_distance_to_degrees(distance)
+    move_for_degrees(degrees)
 async def pivot_turn_right(degrees, motor_speed):
     motor_degrees = int((PIVOT_CIRCUMFERENCE/WHEEL_CIRCUMFERENCE) * abs(degrees))
     await motor_pair.move_for_degrees(motor_pair.PAIR_1, motor_degrees, 50, velocity=motor_speed)
