a1766f0e79bd97eeca8b5dca20a566ec121a64a1
ravinder Mittal <mittal.ravinder@gmail.com>
2025-11-15 19:51:13 -0800

    Update Nov 15 for some missions

--- pybricks/Missions_10_23.py
+++ pybricks/Missions_10_23.py
@@ -4,18 +4,18 @@ from robot import *
 def missions1and2():
     #reset_arms()
     move_straight_gyro(740, DriveSpeed.PUSHING)
-    left_arm_up(180, ArmSpeed.COLLECT)
+    left_arm_up(240, ArmSpeed.COLLECT)
     wait(500)
     move_straight_gyro(-10, DriveSpeed.PUSHING)
     spin_turn(-45)
-    move_straight_gyro(100, DriveSpeed.PRECISE)
-    move_straight_gyro(-100, DriveSpeed.PRECISE)
+    move_straight_gyro(80, DriveSpeed.PRECISE)
+    move_straight_gyro(-80, DriveSpeed.PRECISE)
     spin_turn(45)
-    move_straight_gyro(-730, DriveSpeed.PUSHING)
+    move_straight_gyro(-720, DriveSpeed.PUSHING)
 
 #start on the 12 squares from the left
 def mission1_Part2():
-    move_straight_gyro(425, DriveSpeed.PUSHING)
+    move_straight_gyro(445, DriveSpeed.PUSHING)
     move_straight_gyro(180, DriveSpeed.PRECISE)
     spin_turn(-90)
     move_straight_gyro(90, DriveSpeed.PRECISE)
@@ -25,22 +25,22 @@ def mission1_Part2():
     spin_turn(90)
     move_straight_gyro(-605, DriveSpeed.PUSHING)
 
-#align at square 8
+#align at square 12
 def missions3():
     move_straight_gyro(400, DriveSpeed.PUSHING)
-    move_straight_gyro(280, DriveSpeed.PRECISE)
+    move_straight_gyro(270, DriveSpeed.PRECISE)
     spin_turn(90, TurnSpeed.PRECISE)
-    move_straight_gyro(150, DriveSpeed.PUSHING)
+    move_straight_gyro(50, DriveSpeed.PUSHING)
     move_straight_gyro(180, DriveSpeed.PRECISE)
     spin_turn(-90, TurnSpeed.PRECISE)
     left_arm_up(300, ArmSpeed.DELICATE)
     wait(1000)
     left_arm_down(270, ArmSpeed.DELICATE)
     spin_turn(90, TurnSpeed.PRECISE)
-    move_straight_gyro(-150, DriveSpeed.PUSHING)
+    move_straight_gyro(-50, DriveSpeed.PUSHING)
     move_straight_gyro(-180, DriveSpeed.PRECISE)
     spin_turn(-90, TurnSpeed.PRECISE)
-    move_straight_gyro(-680, DriveSpeed.PUSHING)
+    move_straight_gyro(-660, DriveSpeed.PUSHING)
     
 #Align at square 2
 def missions12Pull():
@@ -68,21 +68,42 @@ def boulder_mission():
     
 #Align at square 6 from left
 def marketplace_flippy_and_boulder_mission():
-    move_straight_gyro(590, DriveSpeed.PUSHING)
-    move_straight_gyro(-100, DriveSpeed.PUSHING)
-    spin_turn(-15, TurnSpeed.ALIGNMENT)
-    move_straight_gyro(200, DriveSpeed.PRECISE)
-    right_arm_down(70, ArmSpeed.DELICATE)
-    #move_straight_gyro(-710, DriveSpeed.PUSHING)
+    
+    move_straight_gyro(600, DriveSpeed.PUSHING)
+    wait(500)
+    move_straight_gyro(-100, DriveSpeed.PRECISE)
+    spin_turn(-90, TurnSpeed.PRECISE)
+    move_straight_gyro(50, DriveSpeed.PRECISE)
+    spin_turn(+90, TurnSpeed.PRECISE)
+    move_straight_gyro(160, DriveSpeed.PRECISE)
+    right_arm_down(90, ArmSpeed.DELICATE)
+    move_straight_gyro(-710, DriveSpeed.PUSHING)
+
+def DisplayFunctions():
+    hub.display.text("Missions 1 and 2")
+    # Display a single letter (e.g., 'H')
+    hub.display.text("Hello")
+
+    # Display a number (e.g., 42)
+    #hub.display.number(42)
+    #wait(1000)
+
+    # Display a sequence of characters, one by one
+    #hub.display.scroll("HELLO", on=500, off=2000) # on/off duration in ms
 
 
 if __name__ == "__main__":
     """
     Main execution - run different demos
     """
+    hub = PrimeHub()
+    #hub.display.text("1")
     #missions1and2()
-    #mission1_Part2
+    #hub.display.text("2-1")
+    #mission1_Part2()
     #missions3()
     #missions12Pull()
     #missions12Push()
+
+    hub.display.text("7")
     marketplace_flippy_and_boulder_mission()
